580e7b35295b30f6e932d44972b914f42e57ce3e
zhenjun <lzj@hel.(none)>
2012-12-09 22:47:03 +0800

    add solutions

--- src/clojure_99_problems/core.clj
+++ src/clojure_99_problems/core.clj
@@ -57,7 +57,7 @@
          (recur (rest lst) (cons (first lst) coll)))))
 (defn my-reverse2 [lst]
   (loop [[f & r :as c] lst coll '()]
-    (if (empty? c) acc
+    (if (empty? c) c
         (recur r (cons f coll)))))
 
 ;; 6
@@ -92,79 +92,161 @@
 ;; Example:
 ;; * (compress '(a a a a b c c a a d e e e e))
 ;; (A B C A D E)
+(defn compress [lst]
+  (loop [[f & r] lst res {}]
+    (if (nil? f) (vals res)
+        (recur r (assoc res f f)))))
+
+
 ;; P09 (**) Pack consecutive duplicates of list elements into sublists.
 ;; If a list contains repeated elements they should be placed in separate sublists.
-
 ;; Example:
 ;; * (pack '(a a a a b c c a a d e e e e))
 ;; ((A A A A) (B) (C C) (A A) (D) (E E E E))
+
+;; how to think recusivly(one step and one part step)
+(defn pack-list [lst]
+  (if (= lst nil)
+    nil
+    (cons (take-while (fn [x] (= x (first lst))) lst)
+	  (pack-list (drop-while (fn [x] (= x (first lst))) lst)))))
+
+(defn pack [lst]
+  (loop [xs lst res '()]
+    (cond
+     (empty? xs) res
+     :else (recur (drop-while (fn [x] (= x (first xs))) xs)
+                  (cons (take-while (fn [x] (= x (first xs))) xs) res)))))
+
 ;; P10 (*) Run-length encoding of a list.
 ;; Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
-
 ;; Example:
 ;; * (encode '(a a a a b c c a a d e e e e))
 ;; ((4 A) (1 B) (2 C) (2 A) (1 D)(4 E))
+(defn encode [lst]
+  (loop [xs lst res '()]
+    (cond
+     (empty? xs) res
+     :else (recur (drop-while (fn [x] (= x (first xs))) xs)
+                  (cons (let [tmp (take-while (fn [x] (= x (first xs))) xs)]
+                          (list (count tmp) (first tmp))) res)))))
+
 ;; P11 (*) Modified run-length encoding.
 ;; Modify the result of problem P10 in such a way that if an element has no duplicates it is simply copied into the result list. Only elements with duplicates are transferred as (N E) lists.
-
 ;; Example:
 ;; * (encode-modified '(a a a a b c c a a d e e e e))
 ;; ((4 A) B (2 C) (2 A) D (4 E))
+(defn encode-modified [lst]
+  ((fn [xs]
+     (loop [[f & r] xs res '()]
+       (if (empty? r) res
+           (recur r (cons (if (= 1 (first f)) (nth f 1)
+                     f) res))))) (encode lst)))
 ;; P12 (**) Decode a run-length encoded list.
 ;; Given a run-length code list generated as specified in problem P11. Construct its uncompressed version.
+(defn decode [lst]
+  (loop [[f & r]lst res '()]
+       (if (and (nil? f) (empty? r)) res
+           (recur r (cons (if (coll? f) (repeat (first f) (last f))
+                              f) res)))))
+
 ;; P13 (**) Run-length encoding of a list (direct solution).
 ;; Implement the so-called run-length encoding data compression method directly. I.e. don't explicitly create the sublists containing the duplicates, as in problem P09, but only count them. As in problem P11, simplify the result list by replacing the singleton lists (1 X) by X.
-
 ;; Example:
 ;; * (encode-direct '(a a a a b c c a a d e e e e))
 ;; ((4 A) B (2 C) (2 A) D (4 E))
+(defn encode-direct [lst]
+  ((fn [xs accum]
+     (if (= nil xs)
+       accum
+       (recur (drop-while (fn [x] (= x (first xs))) xs)
+	      (let [items (take-while (fn [x] (= x (first xs))) xs)]
+		(if (= 1 (count items))
+		  (concat accum items)
+		  (concat accum (list (list (count items) (first items))))))))) lst nil))
+
 ;; P14 (*) Duplicate the elements of a list.
 ;; Example:
 ;; * (dupli '(a b c c d))
 ;; (A A B B C C C C D D)
+(defn dupli [lst]
+  (mapcat (fn [x] (list x x)) lst))
+
+
 ;; P15 (**) Replicate the elements of a list a given number of times.
 ;; Example:
 ;; * (repli '(a b c) 3)
 ;; (A A A B B B C C C)
+(defn repli [lst times]
+  (mapcat (fn [x] (repeat times x)) lst))
 
 ;; P16 (**) Drop every N'th element from a list.
 ;; Example:
 ;; * (drop '(a b c d e f g h i k) 3)
 ;; (A B D E G H K)
+(defn drop-nth [lst n]
+  (let [dlst
+        (loop [xs lst res '()]
+          (if (empty? xs) res
+              (recur (drop n xs) (cons (take (dec n) xs) res))))]
+    (my-flatten dlst)))
+
 ;; P17 (*) Split a list into two parts; the length of the first part is given.
 ;; Do not use any predefined predicates.
-
 ;; Example:
 ;; * (split '(a b c d e f g h i k) 3)
 ;; ( (A B C) (D E F G H I K))
+(defn split [lst n]
+  (list (take n lst) (drop n lst)))
+
 ;; P18 (**) Extract a slice from a list.
 ;; Given two indices, I and K, the slice is the list containing the elements between the I'th and K'th element of the original list (both limits included). Start counting the elements with 1.
-
 ;; Example:
 ;; * (slice '(a b c d e f g h i k) 3 7)
 ;; (C D E F G)
+(defn slice [lst b e]
+  (drop (dec b) (take e lst)))
+
+
 ;; P19 (**) Rotate a list N places to the left.
 ;; Examples:
 ;; * (rotate '(a b c d e f g h) 3)
 ;; (D E F G H A B C)
-
 ;; * (rotate '(a b c d e f g h) -2)
 ;; (G H A B C D E F)
-
 ;; Hint: Use the predefined functions length and append, as well as the result of problem P17.
+(defn rotate [lst n]
+  (if (> n 0) (take (count lst) (drop n (cycle lst)))
+      (take (count lst) (drop (- (count lst) (Math/abs n)) (cycle lst)))))
+
+(defn rotate-1 [lst n]
+  (if (> n 0) (my-flatten (list (drop n lst) (take n lst)))
+      (my-flatten (list (drop (- (count lst) (Math/abs n)) lst) (take (- (count lst) (Math/abs n)) lst)))))
+
 ;; P20 (*) Remove the K'th element from a list.
 ;; Example:
 ;; * (remove-at '(a b c d) 2)
 ;; (A C D)
+(defn remove-at [lst n]
+  (concat (take (dec n) lst) (drop n lst)))
+
 ;; P21 (*) Insert an element at a given position into a list.
 ;; Example:
 ;; * (insert-at 'alfa '(a b c d) 2)
 ;; (A ALFA B C D)
+(defn insert-at [a lst n]
+  (concat (take (dec n) lst) (list a) (drop (dec n) lst)))
+
+
 ;; P22 (*) Create a list containing all integers within a given range.
 ;; If first argument is smaller than second, produce a list in decreasing order.
 ;; Example:
 ;; * (range 4 9)
 ;; (4 5 6 7 8 9)
+(defn my-range [l r]
+  (take (- r (dec l)) (iterate inc l)))
+
+
 ;; P23 (**) Extract a given number of randomly selected elements from a list.
 ;; The selected items shall be returned in a list.
 ;; Example:
